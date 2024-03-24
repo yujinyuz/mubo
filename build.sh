399b6f56a1702948f9cf0ab1aa8ea44fc5e44de0
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-25 00:26:22 +0800

    add wheel setuptools to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # Exit on error
 set -o errexit
 
+pip install setuptools wheel
 python setup.py bdist_wheel
 
 pip install dist/*.whl
 django-project collectstatic --no-input
+
+django-admin migrate --settings=mubo.conf.settings
